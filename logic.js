a15bae09037977a87b27032f4cd80138b486e4b9
ojfordinner <124970715+ojfordinner@users.noreply.github.comian.ouyoung@gmail.com>
2023-03-19 17:52:33 -0400

    Added JS for number btns

--- logic.js
+++ logic.js
@@ -25,3 +25,30 @@ function operate(x,y,z){
         return divide(x,y);
     } return;
 }
+
+const numArray = [];
+function showNum(e){
+    e.stopPropagation()
+    numArray.push(e.target.innerText);
+    disNum = numArray.join('');
+    display.innerText = disNum;
+}
+
+function opHere(e){
+    if(e.target.innerText == 'C'){
+        display.innerText = ''
+        numArray.length = 0
+    }
+}
+
+
+numBtns = document.querySelectorAll('.numbers')
+opBtns = document.querySelectorAll('.operators')
+allBtns = document.querySelectorAll('button')
+display = document.querySelector('.display')
+
+numBtns.forEach(button => 
+    button.addEventListener('click',showNum))
+
+opBtns.forEach(button => 
+    button.addEventListener('click',opHere))
